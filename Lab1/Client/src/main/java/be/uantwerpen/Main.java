287ac16a18856c6276239c1fd7a0c3f2c9a6df1b
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2024-02-15 12:27:32 +0100

    Updated client to also allow sending of text to server.

--- Lab1/Client/src/main/java/be/uantwerpen/Main.java
+++ Lab1/Client/src/main/java/be/uantwerpen/Main.java
@@ -1,11 +1,9 @@
 package be.uantwerpen;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,9 +23,21 @@ public class Main {
         System.out.println("Attempting connection to " + args[0] + ":" + args[1]);
 
         try (Socket socket = new Socket(hostname, port)) {
+            // Data to server
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            Scanner scanner = new Scanner(System.in);
+
+            // Data from server
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-            System.out.println(reader.readLine());
+
+            while (true) {
+                System.out.print("Write something: ");
+                String text = scanner.nextLine();
+                writer.println(text);
+                System.out.println(reader.readLine());
+            }
         } catch (UnknownHostException e) {
             System.out.println("ERROR: Server not found! " + e.getMessage());
         } catch (IOException e) {
