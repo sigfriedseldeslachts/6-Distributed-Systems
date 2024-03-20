3e582d1e8e00744c5ed3d71bdc9cba6b25615a21
Sigfried <me@sigfriedseldeslachts.be>
2024-03-20 17:45:58 +0100

    Added simple request function

---
+++ Project/NamingServer/src/main/java/be/uantwerpen/namingserver/controllers/FileController.java
@@ -0,0 +1,28 @@
+package be.uantwerpen.namingserver.controllers;
+
+import be.uantwerpen.namingserver.models.Node;
+import be.uantwerpen.namingserver.services.NodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    @Autowired
+    private NodeService nodeService;
+
+    @GetMapping("{filename}")
+    public Node show(@PathVariable String filename)
+    {
+        if (filename == null) { // null == "null" is true
+            throw new IllegalArgumentException("Filename cannot be null");
+        }
+
+        return nodeService.getNodeToStoreFileOn(filename);
+    }
+
+}
