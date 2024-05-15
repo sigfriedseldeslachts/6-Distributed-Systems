7fee0ea76dc28b4b7ac497d675e0c84e805684f9
Sigfried <me@sigfriedseldeslachts.be>
2024-05-15 16:58:09 +0200

    Updated the file replication storing code and some small style changes

--- Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/controllers/FileController.java
+++ Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/controllers/FileController.java
@@ -1,38 +1,27 @@
 package be.uantwerpen.distributedclients.controllers;
 
 import be.uantwerpen.distributedclients.services.FileService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
+
     private final FileService fileService;
 
-    public FileController(FileService fileservice) {
-        this.fileService = fileservice;
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
     }
 
     // dit is wat de receiving node moet doen
     @PostMapping("/replication")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) throws IOException {
-
+    public ResponseEntity<Object> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         fileService.store(file);
-        System.out.println("storing file");
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
+        return ResponseEntity.noContent().build();
     }
-
-
 
 }
--- Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/services/FileService.java
+++ Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/services/FileService.java
@@ -21,11 +21,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -33,20 +33,30 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @Service
 @EnableAsync
 public class FileService {
-
-    private final RestClient.Builder restClientBuilder;
-    private Logger logger = LoggerFactory.getLogger(FileService.class);
-    private InfoService infoService;
-    private HashMap<Integer, File> fileList = new HashMap<>();
-    private Map<Integer, Integer> map;
-    private Environment env;
+    private final Logger logger = LoggerFactory.getLogger(FileService.class);
+    private final InfoService infoService;
+    private final HashMap<Integer, File> fileList = new HashMap<>();
     private final RestTemplate restTemplate;
+    private final String dataPath;
 
     public FileService(InfoService infoService, Environment env, RestClient.Builder restClientBuilder) {
         this.infoService = infoService;
-        this.env = env;
-        this.restClientBuilder = restClientBuilder;
+        this.dataPath = env.getProperty("app.directory");
         this.restTemplate = new RestTemplateBuilder().build();
+
+        if (this.dataPath == null) {
+            throw new IllegalArgumentException("app.directory is not set in application.properties");
+        }
+
+        // Create directory if it does not exist
+        File directory = new File(this.dataPath);
+        if (!directory.exists()) {
+            if (directory.mkdirs()) {
+                logger.info("Created directory: {}", this.dataPath);
+            } else {
+                logger.error("Failed to create directory: {}", this.dataPath);
+            }
+        }
     }
 
     // dit is de node die zijn request stuurt naar de receiving node
@@ -60,56 +70,63 @@ public class FileService {
                 .baseUrl(infoService.getNamingServerAddress() + "/files/replication")
                 .defaultHeader("Content-Type", "application/json")
                 .build();
-        this.map = client.method(HttpMethod.POST).body(new ArrayList<>(fileList.keySet())).retrieve().body(new ParameterizedTypeReference<>() {});
-        System.out.println(map);
+        Map<Integer, Integer> map = client.method(HttpMethod.POST).body(new ArrayList<>(fileList.keySet())).retrieve().body(new ParameterizedTypeReference<>() {});
 
         for (Integer hashFile : map.keySet()) {
             int node = map.get(hashFile);
+            if (node == infoService.getSelfNode().hashCode()) {
+                continue;
+            }
 
-            if (node != infoService.getSelfNode().hashCode()) {
-                String nodeAddress = infoService.getNodes().get(node).getSocketAddress();
-                File file = fileList.get(hashFile);
+            String nodeAddress = infoService.getNodes().get(node).getSocketAddress();
+            File file = fileList.get(hashFile);
 
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-                MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-                body.add("file", new FileSystemResource(file));
-                HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-                ResponseEntity<String> responseEntity = restTemplate.exchange(
-                        "http://" + nodeAddress + "/files/replication",
-                        HttpMethod.POST,
-                        requestEntity,
-                        String.class
-                );
-                if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                    System.out.println("File uploaded successfully.");
-                } else {
-                    System.out.println("Failed to upload file. Status code: " + responseEntity.getStatusCode());
-                }
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("file", new FileSystemResource(file));
+            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    "http://" + nodeAddress + "/files/replication",
+                    HttpMethod.POST,
+                    requestEntity,
+                    String.class
+            );
+
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                logger.debug("File uploaded successfully: {}", file.getName());
             } else {
-                System.out.println("SKIP");
+                logger.debug("Failed to upload file. Status code: {}. File name: {}", responseEntity.getStatusCode(), file.getName());
             }
         }
 
     }
 
-
     @Async
     @Scheduled(fixedRate = 5000)
     public void update(){
-        File directory = new File(this.env.getProperty("app.directory"));
+        File directory = new File(this.dataPath);
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
-                fileList.put(HashingFunction.getHashFromString(file.getName()),file);
+                fileList.put(HashingFunction.getHashFromString(file.getName()), file);
             }
         }
     }
 
     public void store(MultipartFile file) throws IOException {
-        File directory = new File(this.env.getProperty("app.directory") + "\\" + file.getOriginalFilename());
-        logger.info("Storing file " + file.getOriginalFilename());
-        file.transferTo(directory);
-    }
+        File targetFile = new File(this.dataPath, Objects.requireNonNull(file.getOriginalFilename()));
+        logger.info("Storing file: {}", targetFile.getAbsoluteFile());
 
+        InputStream initialStream = file.getInputStream();
+        byte[] buffer = new byte[initialStream.available()];
+        initialStream.read(buffer);
+
+        try (OutputStream outStream = new FileOutputStream(targetFile)) {
+            outStream.write(buffer);
+        } catch (Exception e) {
+            logger.error("Failed to store file: {}", e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
