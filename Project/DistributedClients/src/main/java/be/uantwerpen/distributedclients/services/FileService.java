987f5f2e00325ab33f2567a94f6757171d0c3509
GoranVanNimmen <goran.van.nimmen@gmail.com>
2024-05-15 16:41:19 +0200

    changes for sending file

--- Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/services/FileService.java
+++ Project/DistributedClients/src/main/java/be/uantwerpen/distributedclients/services/FileService.java
@@ -5,15 +5,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,11 +40,13 @@ public class FileService {
     private HashMap<Integer, File> fileList = new HashMap<>();
     private Map<Integer, Integer> map;
     private Environment env;
+    private final RestTemplate restTemplate;
 
     public FileService(InfoService infoService, Environment env, RestClient.Builder restClientBuilder) {
         this.infoService = infoService;
         this.env = env;
         this.restClientBuilder = restClientBuilder;
+        this.restTemplate = new RestTemplateBuilder().build();
     }
 
     // dit is de node die zijn request stuurt naar de receiving node
@@ -61,19 +69,23 @@ public class FileService {
             if (node != infoService.getSelfNode().hashCode()) {
                 String nodeAddress = infoService.getNodes().get(node).getSocketAddress();
                 File file = fileList.get(hashFile);
-                client = RestClient.builder()
-                        .baseUrl("http://" + nodeAddress + "/files/replication")
-                        .defaultHeader("Content-Type", "application/json")
-                        .build();
 
-                ResponseEntity<Void> response = client.post()
-                        .contentType(APPLICATION_JSON)
-                        .body(file)
-                        .retrieve()
-                        .onStatus(status -> !status.is2xxSuccessful(), (request, resp) -> logger.error("Failed to upload file: status code: {}", resp.getStatusCode()))
-                        .toBodilessEntity();
-
-                logger.info("Sending file " + file.getAbsolutePath());
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+                MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+                body.add("file", new FileSystemResource(file));
+                HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+                ResponseEntity<String> responseEntity = restTemplate.exchange(
+                        "http://" + nodeAddress + "/files/replication",
+                        HttpMethod.POST,
+                        requestEntity,
+                        String.class
+                );
+                if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                    System.out.println("File uploaded successfully.");
+                } else {
+                    System.out.println("Failed to upload file. Status code: " + responseEntity.getStatusCode());
+                }
             } else {
                 System.out.println("SKIP");
             }
@@ -95,8 +107,8 @@ public class FileService {
     }
 
     public void store(MultipartFile file) throws IOException {
-        File directory = new File(this.env.getProperty("app.directory") + "\\" + file.getName());
-        logger.info("Storing file " + file.getName());
+        File directory = new File(this.env.getProperty("app.directory") + "\\" + file.getOriginalFilename());
+        logger.info("Storing file " + file.getOriginalFilename());
         file.transferTo(directory);
     }
 
